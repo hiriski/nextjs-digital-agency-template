46459b4180d26c1089163e864700d0b896cacd3a
hiriski <hiriski@outlook.com>
2025-06-01 20:03:48 +0700

    feat(core components): added `StyledRoundedButton` component

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,3 +1,4 @@
 export { default as Typography } from './typography'
 export { default as SectionTitle } from './section-title'
 export { default as StyledButton } from './styled-button'
+export { default as StyledRoundedButton } from './styled-rounded-button'
